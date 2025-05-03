0f5a7c850568054b34bdab5e9d40809da47f3cba
cheog0 <clf0914@naver.com>
2025-05-04 03:27:08 +0900

    facorites file

---
+++ utils/favorites.js
@@ -0,0 +1,32 @@
+import { showToast } from "./toasts.js";
+import { renderPopularMovies, renderFavorites } from "../main.js";
+// favorites.js
+
+export function getFavorites() {
+  return JSON.parse(localStorage.getItem("favorites")) || [];
+}
+
+export function saveFavorites(favorites) {
+  return localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+export function toggleFavorite(movie, favorites) {
+  const index = favorites.findIndex((fav) => fav.id === movie.id);
+
+  if (index === -1) {
+    // 즐겨찾기에 추가
+    favorites.push(movie);
+    showToast(`"${movie.title}" 영화를 찜했습니다!`);
+  } else {
+    // 즐겨찾기에서 제거
+    favorites.splice(index, 1);
+    showToast(`"${movie.title}" 영화를 찜 목록에서 제거했습니다.`);
+  }
+
+  // 로컬 스토리지 업데이트
+  saveFavorites(favorites);
+
+  // UI 업데이트
+  renderPopularMovies();
+  renderFavorites();
+}
